c79c1d64176e8240dd18be0faeb6150bbdc8cf06
Hzpa <hz_houzhe@163.com>
2018-09-04 17:14:26 +0800

    add spring cloud config

---
+++ config-client/src/main/java/com/pale/springcloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,19 @@
+package com.pale.springcloud.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Author: houzhe
+ * Date: 2018-09-04
+ * Description:
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ config-server/src/main/java/com/pale/springcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.pale.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Author: houzhe
+ * Date: 2018-09-04
+ * Description:
+ **/
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
